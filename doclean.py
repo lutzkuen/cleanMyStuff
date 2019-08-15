57de8fa5906c6caae2e1fc6bd12bb8c02c1c7105
lutzkuen <lutz.kuenneke89@gmail.com>
2019-08-15 20:09:05 +0200

    win client

--- doclean.py
+++ doclean.py
@@ -1,6 +1,6 @@
 from stuff_cleaner import cleaner
 
-base_path = 'C:\\Users\\lutz.kuenneke\\Documents'
+base_path = '..\\..\\..'
 
 cl = cleaner.CrawlerMaster(base_path)
 
---
+++ stuff_cleaner/file_crawler.py
@@ -0,0 +1,82 @@
+"""
+Author: Lutz Künneke
+Date: 2019-089-10
+
+This file contains the main classes that power the crawler
+"""
+
+import os
+import sys
+import logging
+import requests
+import hashlib
+
+def md5(fname):
+    hash_md5 = hashlib.md5()
+    with open(fname, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_md5.update(chunk)
+    return hash_md5.hexdigest()
+
+
+class CrawlerSlave(object):
+
+    def __init__(self, directory, master):
+        self.directory = directory
+        self.file_endings = []
+        self.logger = logging.getLogger()
+        self.at_arr = []
+        try:
+            files = os.listdir(directory)
+        except Exception as e:
+            self.logger.error(str(e))
+            return
+        for file in files:
+            try:
+                fullname = os.path.join(directory, file)
+                if os.path.isfile(fullname):
+                    master.save_file(fullname)
+                else:
+                    slave = CrawlerSlave(fullname, master)
+            except Exception as e:
+                print(str(e))
+                raise(e)
+
+
+class CrawlerMaster(object):
+
+    def __init__(self, directory):
+        self.logger = logging.getLogger()
+        self.logger.setLevel(logging.ERROR)
+        handler = logging.StreamHandler(sys.stdout)
+        logFormatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
+        handler.setFormatter(logFormatter)
+        self.logger.addHandler(handler)
+        self.root = directory
+        self.current = self.root
+
+    def save_file(self, filename):
+        post_url = 'http://127.0.0.1:5000/v1/files'
+        new_file = {
+            'path': filename,
+            'content': md5(filename),
+            'device': 'HDD'
+        }
+        r = requests.post(url=post_url, json=new_file)
+        if r.status_code == 201:
+            self.logger.info('Posted {path} to API'.format(path=filename))
+        else:
+            self.logger.warning('Response code {status} while posting {path}'.format(status=r.status_code, path=filename))
+
+    def crawl_root(self):
+        files = os.listdir(self.root)
+        for file in files:
+            fullname = os.path.join(self.root, file)
+            if os.path.isdir(fullname):
+                slave = CrawlerSlave(fullname, self)
+            else:
+                self.save_file(fullname)
+
+if __name__ == '__main__':
+    cl = CrawlerMaster('..\\..\\..')
+    cl.crawl_root()
